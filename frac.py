ff016ffaf1dfb81873571c4b5796379bddd8aefb
ACFleming <flemingoflemingoflemingo@gmail.com>
2021-10-18 23:10:35 +1100

    Part 2 q1 works, with much testing

--- frac.py
+++ frac.py
@@ -7,30 +7,50 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+
+# Reached 100 with python frac_main.py --net full2 --hid 15 --lr 0.001 --init 0.35
+
 class Full2Net(torch.nn.Module):
     def __init__(self, hid):
         super(Full2Net, self).__init__()
+        self._layer1 = nn.Linear(2,hid)
+        self._layer2 = nn.Linear(hid,hid)
+        self._layer_output = nn.Linear(hid,1)
 
     def forward(self, input):
-        self.hid1 = None
-        self.hid2 = None
-        return 0*input[:,0]
+
+        self.hid1 = torch.tanh(self._layer1(input))
+        self.hid2 = torch.tanh(self._layer2(self.hid1))
+        input = torch.sigmoid(self._layer_output(self.hid2))
+        return input[:,0].view(-1,1)
 
 class Full3Net(torch.nn.Module):
     def __init__(self, hid):
         super(Full3Net, self).__init__()
+        self._layer1 = nn.Linear(2,hid)
+        self._layer2 = nn.Linear(hid,hid)
+        self._layer3 = nn.Linear(hid,hid)
+        self._layer_output = nn.Linear(hid,1)
 
     def forward(self, input):
-        self.hid1 = None
-        self.hid2 = None
-        self.hid3 = None
-        return 0*input[:,0]
+        self.hid1 = torch.tanh(self._layer1(input))
+        self.hid2 = torch.tanh(self._layer2(self.hid1))
+        self.hid3 = torch.tanh(self._layer3(self.hid2))
+        input = torch.sigmoid(self._layer_output(self.hid3))
+        return input[:,0].view(-1,1)
 
 class DenseNet(torch.nn.Module):
-    def __init__(self, num_hid):
+    def __init__(self, hid):
         super(DenseNet, self).__init__()
+        self._layer1 = nn.Linear(2,hid)
+        self._layer2 = nn.Linear(hid,hid)
+        self._layer_output = nn.Linear(hid,1)
 
     def forward(self, input):
-        self.hid1 = None
-        self.hid2 = None
-        return 0*input[:,0]
+        self.hid1 = torch.tanh(self._layer1(input))
+        self.hid2 = torch.tanh(torch.cat(   self._layer2(input),\
+                                            self._layer2(self.hid1),dim=1))
+        input = torch.sigmoid(torch.cat(    self._layer_output(input),\
+                                            self._layer_output(self.hid1),\
+                                            self._layer_output(self.hid2), dim=1))
+        return input[:,0].view(-1,1)
