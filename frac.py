e43ae899879e5f96b52f330a20a63979bfb8be49
ACFleming <flemingoflemingoflemingo@gmail.com>
2021-10-19 22:06:15 +1100

    Dense net works for p2q3

--- frac.py
+++ frac.py
@@ -17,6 +17,7 @@ class Full2Net(torch.nn.Module):
         self._layer2 = nn.Linear(hid,hid)
         self._layer_output = nn.Linear(hid,1)
 
+
     def forward(self, input):
 
         self.hid1 = torch.tanh(self._layer1(input))
@@ -41,18 +42,26 @@ class Full3Net(torch.nn.Module):
         input = torch.sigmoid(self._layer_output(self.hid3))
         return input[:,0].view(-1,1)
 
+
+# Reached 100 with python frac_main.py --net dense --hid 15 --init 0.35
 class DenseNet(torch.nn.Module):
     def __init__(self, hid):
         super(DenseNet, self).__init__()
         self._layer1 = nn.Linear(2,hid)
-        self._layer2 = nn.Linear(hid,hid)
-        self._layer_output = nn.Linear(hid,1)
+        self._layer2 = nn.Linear(2+ hid,hid)
+        self._layer_output = nn.Linear(2 + hid + hid,1)
 
     def forward(self, input):
+        # print("INPUT:", input.size())
         self.hid1 = torch.tanh(self._layer1(input))
-        self.hid2 = torch.tanh(torch.cat(   self._layer2(input),\
-                                            self._layer2(self.hid1),dim=1))
-        input = torch.sigmoid(torch.cat(    self._layer_output(input),\
-                                            self._layer_output(self.hid1),\
-                                            self._layer_output(self.hid2), dim=1))
-        return input[:,0].view(-1,1)
+        # print("HID 1:", self.hid1.size())
+        input2 = torch.cat((input,self.hid1),dim=1)
+        # print("INPUT 2:", input2.size())
+        self.hid2 = torch.tanh(self._layer2(input2))
+        # print("HID 2", self.hid2.size())
+        input3 = torch.cat((input,self.hid1, self.hid2),dim=1)
+        # print("INPUT 3:", input3.size())
+        output = torch.sigmoid(self._layer_output(input3))
+        # print("OUTPUT:", output.size())
+        return output[:,0].view(-1,1)
+        
